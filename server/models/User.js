175ae65e68b0d40a894f502102186927135548b3
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-09-27 22:48:03 +0200

    change username to name and surname User model

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
     type: String,
     required: true
   },
@@ -18,7 +22,7 @@ const userSchema = new Schema({
   type: {
     type: String,
     required: true,
-    enum: ['Volunteer', '']
+    enum: ['Volunteer', 'Traveler']
   },
   pocket: {
     type: Number,
