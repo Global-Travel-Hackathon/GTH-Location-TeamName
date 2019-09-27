644cea85ae6e97b4c179da5e26fd06f18924d63f
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-09-27 22:38:36 +0200

    fix User Model name

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['Volunteer', '']
+  },
+  pocket: {
+    type: Number,
+  },
+  comments: [{
+    type: ObjectId,
+    ref: 'Comment'
+  }],
+  myTrips: [{
+    type: ObjectId,
+    ref: 'Trip'
+  }],
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
