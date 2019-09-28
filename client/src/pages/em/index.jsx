85c1184d7bfaf154d747d55bce6638c5323e0959
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-09-28 05:41:45 +0200

    add createTrip component to em page

--- client/src/pages/em/index.jsx
+++ client/src/pages/em/index.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import CreateTrip from './../createTrip'
 
 const Em = () => {
+
+  const [ mainState, setMainState ] = useState({
+    createTrip: false,
+  })
+
+  const handleCreateTrip = () => {
+    setMainState({
+      createTrip: !mainState.createTrip
+    })
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {!mainState.createTrip ?
+        <>
+          <h1>Welcome Paquito</h1>
+          <button onClick={() => handleCreateTrip()}>+</button>
+        </>
+      :
+        <>
+          <button onClick={() => handleCreateTrip()}>Back</button>
+          <CreateTrip />
+        </>
+      }
+
+    </>
   )
 }
 
