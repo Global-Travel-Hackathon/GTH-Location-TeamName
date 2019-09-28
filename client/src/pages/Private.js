eca9199f574fa58827a6b9165d444f72395aed07
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-09-28 05:35:27 +0200

    add createTrip to prvate page

--- client/src/pages/Private.js
+++ client/src/pages/Private.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 import { ReactComponent as WikiEm } from '../logotipo.svg'
 import withAuth from '../hoc/withAuth.js';
 
-class Private extends Component {
-  render() {
-    return (
-      <div>
-        <WikiEm className="logo"/>
-        <h1>Welcome Paquito</h1>
-      </div>
-    )
+import CreateTrip from './createTrip'
+
+const Private = (props) => {
+
+  const [ mainState, setMainState ] = useState({
+    createTrip: false,
+  })
+
+  const handleCreateTrip = () => {
+    setMainState({
+      createTrip: !mainState.createTrip
+    })
   }
+
+  return (
+    <>
+      {!mainState.createTrip ?
+        <>
+          <WikiEm className="logo"/>
+          <h1>Welcome Paquito</h1>
+
+          <button onClick={() => handleCreateTrip()}>+</button>
+        </>
+      :
+        <>
+          <button onClick={() => handleCreateTrip()}>Back</button>
+          <CreateTrip />
+        </>
+      }
+
+    </>
+  )
 }
 
 export default withAuth(Private);
